7b3edc44104a430579d5ec2a4b3bee86a53360d5
rogiave12 <63801689+rogiave12@users.noreply.github.com>
2022-07-17 19:17:18 +0200

    Mortgage calculator almost done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using MahApps.Metro;
 using MahApps.Metro.Controls;
 using LiveCharts;
 using LiveCharts.Wpf;
+using System.Text.RegularExpressions;
 
 namespace FinancialPortal
 {
@@ -29,38 +30,82 @@ namespace FinancialPortal
         {
             InitializeComponent();
             DataContext = m;
+
         }
 
-        
+
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
             repayment();
+            switchingQuaters();
 
         }
+        public void switchingQuaters()
+        {
+            switch (repaymentperiod.Text)
+            {
+                case "monthly":
+                    timepayment.Content = "Monthly payment";
+                    break;
+                case "quartely":
+                    timepayment.Content = "Quartely payment";
+                    break;
+                case "half yearly":
+                    timepayment.Content = "Half yearly payment";
+                    break;
+                case "yearly":
+                    timepayment.Content = "Yearly payment";
+                    break;
+                default:
+                    timepayment.Content = "Monthly payment";
+                    break;
+
+            }
+        }
+
         public void repayment() {
-            if((repaymentperiod.Text != null)&&(loanamount.Text!=null) &&(interestrate.Text!=null) &&(year.Text!=null)) { 
-            if (repaymentperiod.Text == "monthly")
-            {
-                m.Calculate(double.Parse(loanamount.Text), double.Parse(interestrate.Text), 12,double.Parse(year.Text));
+            var regex = new Regex("(([A-Z])|([a-z])|([ ]))");
+
+            if ((regex.IsMatch(repaymentperiod.Text) == false) && ((regex.IsMatch(loanamount.Text) == false) && (regex.IsMatch(interestrate.Text) == false) && (regex.IsMatch(year.Text) == false))){
+
+
+                if ((repaymentperiod.Text != null) && (loanamount.Text != null) && (interestrate.Text != null) && (year.Text != null))
+                {
+                    if (repaymentperiod.Text == "monthly")
+                    {
+                        m.Calculate(double.Parse(loanamount.Text), double.Parse(interestrate.Text), 1, double.Parse(year.Text));
+                    }
+                    if (repaymentperiod.Text == "quartely")
+                    {
+                        m.Calculate(double.Parse(loanamount.Text), double.Parse(interestrate.Text), 3, double.Parse(year.Text));
+                    }
+                    if (repaymentperiod.Text == "half yearly")
+                    {
+                        m.Calculate(double.Parse(loanamount.Text), double.Parse(interestrate.Text), 6, double.Parse(year.Text));
+                    }
+                    if (repaymentperiod.Text == "yearly")
+                    {
+                        m.Calculate(double.Parse(loanamount.Text), double.Parse(interestrate.Text), 12, double.Parse(year.Text));
+                    }
+                }
+                else
+                {
+                    hint.Text = "*Not selected a repayment period";
+                }
             }
-            if (repaymentperiod.Text == "quartely")
-            {
-                m.Calculate(double.Parse(loanamount.Text), double.Parse(interestrate.Text), 4, double.Parse(year.Text));
-            }
-            if (repaymentperiod.Text == "yearly")
-            {
-                m.Calculate(double.Parse(loanamount.Text), double.Parse(interestrate.Text), 1, double.Parse(year.Text));
-            }
-            }
+
             else
             {
-                hint.Text = "*Not selected a repayment period";
+                hint.Text = "*Text must only contain numbers";
             }
         }
+    
 
-
-
+        private void periodchanging(object sender, RoutedEventArgs e)
+        {
+            switchingQuaters();
+        }
     }
 }
--- Mortgage.cs
+++ Mortgage.cs
@@ -23,31 +23,68 @@ namespace FinancialPortal
         }
         public void Calculate(double loanamount, double interestrate, double interval, double years)
         {
-            Console.WriteLine("Zadejte co si chcete půjčit:");
-            double zaklad = double.Parse(Console.ReadLine());
-            Console.WriteLine("Zadjete úrok:");
-            double urok = ((double.Parse(Console.ReadLine()) / 100) / 12);
-            Console.WriteLine("Počet let:");
-            double pocetlet = double.Parse(Console.ReadLine()) * 12;
+            https://www.wikihow.com/Calculate-Mortgage-Payments
+            try { 
+            interestrate = (interestrate/ 100) / 12;
+            years *= 12;
+
+            double total = 1 + interestrate;
+            
+            double upperformula = interestrate * Math.Pow(total, years);
+            double lowerformula = Math.Pow(total, years) - 1;
+            double subtotal = upperformula / lowerformula;
+            double final = loanamount * subtotal;
+            
+
+            if (interval == 12)
+            {
+                final *= 12;
+                Totalypaid = final * (years/12);
+                Totalypaid = Math.Round(Totalypaid, 2);
+                final =Math.Round(final,2);
+                MonthlyPayment= final;
+                Change("MonthlyPayment");
+                
+                Change("Totalypaid");
+            }
+            if (interval == 3)
+            {
+                final *= 3;
+                Totalypaid = (final * 4) * (years / 12);
+                Totalypaid = Math.Round(Totalypaid,2);
+                final =Math.Round(final, 2);
+                MonthlyPayment = final;
+                Change("MonthlyPayment");
+                Change("Totalypaid");
+
+            }
+            if(interval == 6)
+            {
+                final *= 6;
+                Totalypaid = (final * 2) * (years / 12);
+                Totalypaid = Math.Round(Totalypaid, 2);
+                final =Math.Round(final, 2);
+                MonthlyPayment = final;
+                Change("MonthlyPayment");             
+                Change("Totalypaid");
+            }
+            if (interval == 1)
+            {
+                Totalypaid = (final * 12) * (years / 12);
+                Totalypaid = Math.Round(Totalypaid, 2);
+                final =Math.Round(final, 2);
+                MonthlyPayment = final;
+                Change("MonthlyPayment");
+                Change("Totalypaid");
+
+            }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
 
-            Console.WriteLine("Kolik si chcete půjčit:" + zaklad);
-            Console.WriteLine("úrok:" + urok);
-            Console.WriteLine("Počet měsíců:" + pocetlet);
-            double mezi = 1 + urok;
-
-            double horni = urok * Math.Pow(mezi, pocetlet);
-            double spodni = Math.Pow(mezi, pocetlet) - 1;
-            double mezisoucet = horni / spodni;
-            double vysledek = zaklad * mezisoucet;
-            Console.WriteLine(horni);
-            Console.WriteLine(spodni);
-            Console.WriteLine(mezisoucet);
-            Console.WriteLine("výsledek měsíční" + vysledek);
-            Console.WriteLine("výsledek půlroční" + (vysledek * 6));
-            Console.WriteLine("výsledek roční" + (vysledek * 12));
-            Console.WriteLine("Výsledek kvartální" + (vysledek * 3));
-            Console.ReadKey();
 
         }
     }
