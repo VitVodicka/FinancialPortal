e99aa470c95c83a81a4035b002376222b9fa924e
rogiave12 <63801689+rogiave12@users.noreply.github.com>
2022-07-29 15:44:27 +0200

    the layout fixed, but the binding dosen t work, the problem is that it isn t binded with hamburger item image

---
+++ Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPortal
+{
+    internal class Account:INotifyPropertyChanged
+    {
+        public double MoneyStatus { get; set; }
+        public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+}
---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPortal
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        
+        public Account Account { get; set; }
+
+        public User(string name, string surname, double moneyStatus, Account account)
+        {
+            Name = name;
+            Surname = surname;
+            
+            Account = account;
+        }
+    }
+}
